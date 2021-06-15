96079d98c822ccd7f06e0de2ac86cf0da542fa8b
Rundong-Li <lirundong16@mails.ucas.edu.cn>
2021-06-15 16:48:40 +0800

    add TransformerEncoder class in model.py

--- modeling/model.py
+++ modeling/model.py
@@ -62,6 +62,167 @@ class TextCNN(Model):
         return x
 
 
+class TransformerEncoder(Model):
+    def __init__(self, 
+                vocab_size, 
+                hid_dim,   # embedding之后词向量的维度
+                n_layers,  # num of EncoderLayer
+                n_heads,   # num of head in MultiHeadAttentionLayer
+                pf_dim,    # pf: positionwise feed forward
+                dropout,
+                device,
+                max_length = 100):
+        super(TransformerEncoder, self).__init__(vocab_size)
+        self.device = device
+        self.tok_embedding = nn.Embedding(vocab_size, hid_dim)
+        self.pos_embedding = nn.Embedding(max_length, hid_dim)
+        self.layers = nn.ModuleList([EncoderLayer(hid_dim, 
+                                                  n_heads, 
+                                                  pf_dim,
+                                                  dropout, 
+                                                  device) 
+                                     for _ in range(n_layers)])
+        self.dropout = nn.Dropout(dropout)
+        self.scale = torch.sqrt(torch.FloatTensor([hid_dim])).to(device)
+        
+    def forward(self, src, src_mask):
+        # src = [batch_size, src_len]
+        # src_mask = [batch_size, 1, 1, src_len]
+        batch_size = src.shape[0]
+        src_len = src.shape[1]
+    
+        pos = torch.arange(0, src_len).unsqueeze(0).repeat(batch_size, 1).to(self.device) 
+        # pos = [batch_size, src_len]
+        src = self.dropout((self.tok_embedding(src) * self.scale) + self.pos_embedding(pos))
+        # src = [batch_size, src_len, hid_dim]
+        
+        for layer in self.layers:
+            src = layer(src, src_mask)
+        # src = [batch_size, src_len, hid_dim]
+            
+        return src
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, 
+                 hid_dim, 
+                 n_heads, 
+                 pf_dim,  
+                 dropout, 
+                 device):
+        super().__init__()
+        
+        self.self_attn_layer_norm = nn.LayerNorm(hid_dim) # layer normalization for self-attention layer
+        self.ff_layer_norm = nn.LayerNorm(hid_dim) # layer normalization for feed-forward layer
+        self.self_attention = MultiHeadAttentionLayer(hid_dim, n_heads, dropout, device)
+        self.positionwise_feedforward = PositionwiseFeedforwardLayer(hid_dim, 
+                                                                     pf_dim, 
+                                                                     dropout)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, src, src_mask):
+        # src = [batch_size, src_len, hid_dim]
+        # src_mask = [batch_size, 1, 1, src_len] 
+                
+        # self attention
+        _src, _ = self.self_attention(src, src, src, src_mask) # query = key = value = src
+
+        # dropout, residual connection and layer norm
+        src = self.self_attn_layer_norm(src + self.dropout(_src))
+        # src = [batch_size, src_len, hid_dim]
+
+        # positionwise feedforward
+        _src = self.positionwise_feedforward(src)
+
+        # dropout, residual and layer norm
+        src = self.ff_layer_norm(src + self.dropout(_src))
+        # src = [batch_size, src_len, hid_dim]
+        
+        return src
+
+
+class MultiHeadAttentionLayer(nn.Module):
+    def __init__(self, hid_dim, n_heads, dropout, device):
+        super().__init__()
+        
+        assert hid_dim % n_heads == 0
+        
+        self.hid_dim = hid_dim
+        self.n_heads = n_heads
+        self.head_dim = hid_dim // n_heads
+        
+        self.fc_q = nn.Linear(hid_dim, hid_dim)
+        self.fc_k = nn.Linear(hid_dim, hid_dim)
+        self.fc_v = nn.Linear(hid_dim, hid_dim)
+        
+        self.fc_o = nn.Linear(hid_dim, hid_dim)
+        
+        self.dropout = nn.Dropout(dropout)
+        
+        self.scale = torch.sqrt(torch.FloatTensor([self.head_dim])).to(device)
+        
+    def forward(self, query, key, value, mask = None):
+        
+        # query = [batch_size, query_len, hid_dim]
+        # key = [batch_size, key_len, hid_dim]
+        # value = [batch_size, value_len, hid_dim]
+        batch_size = query.shape[0]
+                
+        Q = self.fc_q(query)
+        K = self.fc_k(key)
+        V = self.fc_v(value)
+        # Q = [batch_size, query_len, hid_dim]
+        # K = [batch_size, key_len, hid_dim]
+        # V = [batch_size, value_len, hid_dim]
+                
+        Q = Q.view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        K = K.view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        V = V.view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        # Q = [batch_size, n_heads, query_len, head_dim]
+        # K = [batch_size, n_heads, key_len, head_dim]
+        # V = [batch_size, n_heads, value_len, head_dim]
+                
+        energy = torch.matmul(Q, K.permute(0, 1, 3, 2)) / self.scale
+        # energy = [batch_size, n_heads, query_len, key_len]
+        
+        if mask is not None:
+            energy = energy.masked_fill(mask == 0, -1e10)
+        
+        attention = torch.softmax(energy, dim = -1)
+        # attention = [batch_size, n_heads, query_len, key_len]
+        x = torch.matmul(self.dropout(attention), V)
+        # x = [batch_size, n_heads, query_len, head_dim]
+        x = x.permute(0, 2, 1, 3).contiguous()
+        # x = [batch_size, query_len, n_heads, head_dim]
+        x = x.view(batch_size, -1, self.hid_dim)
+        # x = [batch_size, query_len, hid_dim]
+        x = self.fc_o(x)
+        # x = [batch_size, query_len, hid_dim]
+
+        return x, attention
+
+
+class PositionwiseFeedforwardLayer(nn.Module):
+    def __init__(self, hid_dim, pf_dim, dropout):
+        super().__init__()
+
+        self.fc_1 = nn.Linear(hid_dim, pf_dim)
+        self.fc_2 = nn.Linear(pf_dim, hid_dim)
+
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x):
+        # x = [batch_size, seq_len, hid_dim]
+        # x = self.dropout(torch.relu(self.fc_1(x)))
+        # use GELU activation function
+        x = self.dropout(nn.GELU(self.fc_1(x)))
+        # x = [batch_size, seq_len, pf_dim]
+        x = self.fc_2(x)
+        # x = [batch_size, seq_len, hid_dim]
+
+        return x
+
+
 def train(model, dataloader, epoch, optimizer, criterion, scheduler):
     # 定义训练过程
     train_loss, train_acc = 0.0, 0.0
